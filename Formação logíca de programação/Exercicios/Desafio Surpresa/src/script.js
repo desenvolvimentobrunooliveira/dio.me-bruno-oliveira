849172f16aca08bfc556f0a8253230366baff483
desenvolvimentobrunooliveira <desenvolvimentobrstecnologia@gmail.com>
2025-09-02 09:51:20 -0300

    Os Dados dos pokemons, forma inseridos em umamatriz

---
+++ Formação logíca de programação/Exercicios/Desafio Surpresa/src/script.js
@@ -0,0 +1,62 @@
+//desafio surpresa
+//Armazenar o maximo de informações dos pokemons
+//Centro Pokemon
+
+let adestrador = "Noxe"
+let equipeNox = 
+[
+//nome    
+["Poochvene", "Zigzagoon", "Dragonite", "Charmander","Squirtle"],
+
+//level
+[2,4,5,3,1],
+
+//hp
+[13,25,24,15,12],
+
+//sexo
+["M", "F", "M", "F", "F"],
+
+//xp Atual
+[15,75,20,12,95],
+]
+
+
+
+
+console.log("O Pokemons do adestrador: " + adestrador)
+
+console.log("")
+//Poochvene
+console.log("Nome: " + equipeNox [0][0] 
+        + ", Level: " + equipeNox [1][0] 
+        + ", HP atual: " + equipeNox [2][0]
+        + ", Sexo: " + equipeNox [3][0]
+        + ", XP Atual: " + equipeNox [4][0])
+//Zigzagoon
+console.log("Nome: " + equipeNox [0][1] 
+        + ", Level: " + equipeNox [1][1] 
+        + ", HP atual: " + equipeNox [2][1]
+        + ", Sexo: " + equipeNox [3][1]
+        + ", XP Atual: " + equipeNox [4][1])
+//Dragonite
+console.log("Nome: " + equipeNox [0][2] 
+        + ", Level: " + equipeNox [1][2] 
+        + ", HP atual: " + equipeNox [2][2]
+        + ", Sexo: " + equipeNox [3][2]
+        + ", XP Atual: " + equipeNox [4][2])
+//Charmander
+console.log("Nome: " + equipeNox [0][3] 
+        + ", Level: " + equipeNox [1][3] 
+        + ", HP atual: " + equipeNox [2][3]
+        + ", Sexo: " + equipeNox [3][3]
+        + ", XP Atual: " + equipeNox [4][3])
+//Squirtle
+console.log("Nome: " + equipeNox [0][4] 
+        + ", Level: " + equipeNox [1][4] 
+        + ", HP atual: " + equipeNox [2][4]
+        + ", Sexo: " + equipeNox [3][4]
+        + ", XP Atual: " + equipeNox [4][4])
+
+        
+
