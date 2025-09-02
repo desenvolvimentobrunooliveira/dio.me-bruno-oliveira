86d9b39a8973241776f7c32ab51a3858976e8aaf
desenvolvimentobrunooliveira <desenvolvimentobrstecnologia@gmail.com>
2025-09-02 08:32:58 -0300

    Desafio terminado

---
+++ Formação logíca de programação/Desafios de Projeto/src/index.js
@@ -0,0 +1,45 @@
+//desafio - Classificador de nível de Herói
+
+//variavel
+let nomeHeroi = "Nox"
+let xp = 3500
+let nivel = ""
+
+//estrutura de decisão
+
+switch (true){
+    case (xp <= 1000):
+        nivel = "Ferro"
+    break
+    
+    case (xp >= 1001 && xp <= 2000):
+        nivel = "Bronze"
+    break
+
+    case (xp >= 2001 && xp <= 5000):
+        nivel = "Prata"
+    break
+    
+    case (xp >= 5001 && xp <= 7000):
+        nivel = "Ouro"
+    break
+
+    case (xp >= 7001 && xp <= 8000):
+        nivel = "Platina"
+    break
+
+    case (xp >= 8001 && xp <= 9000):
+        nivel = "Ascendente"
+    break
+
+    case (xp >= 9001 && xp <= 10000):
+        nivel = "Imortal"
+    break
+
+    case (xp > 10001):
+        nivel = "Radiante"
+    break
+}
+
+//Mensagem final
+console.log("O Herói de nome " + nomeHeroi +  " está no nível de " + nivel)
